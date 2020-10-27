f60f00aaf1ba469b06abbaea7af1e827d83d3709
Shubham <shubham@zappy.ai>
2020-10-27 12:15:14 +0000

    chnaged cs file

---
+++ NewZappyAction.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel;
+using Zappy.SharedInterface;
+using Zappy.SharedInterface.Helper;
+
+namespace CustomActions
+{
+    public class NewZappyAction : TemplateAction
+    {
+        [Category("Input")]
+        [Description("InputBox Text")]
+        public DynamicProperty<string> Text { get; set; }
+
+        public override void Invoke(IZappyExecutionContext context, ZappyTaskActionInvoker actionInvoker)
+        {
+            //   MessageBox.Show(Text);
+        }
+
+
+        public override string AuditInfo()
+        {
+            return base.AuditInfo() + " Text:" + this.Text;
+        }
+
+
+    }
+}
