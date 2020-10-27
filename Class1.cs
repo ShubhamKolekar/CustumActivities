4b3e650794cd6ccb74dd5052591cfee4c00cce6e
Shubham <shubham@zappy.ai>
2020-10-27 10:07:22 +0000

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel;
+using Zappy.SharedInterface;
+using Zappy.SharedInterface.Helper;
+
+namespace CustomActivities
+{
+    public class NewCustumAction : TemplateAction
+    {
+        [Category("Input")]
+        [Description("InputBox Text")]
+        public DynamicProperty<string> Text { get; set; }
+
+        public override void Invoke(IZappyExecutionContext context, ZappyTaskActionInvoker actionInvoker)
+        {
+          //     MessageBox.Show(Text);
+        }
+
+
+        public override string AuditInfo()
+        {
+            return base.AuditInfo() + " Text:" + this.Text;
+        }
+
+
+    }
+}
